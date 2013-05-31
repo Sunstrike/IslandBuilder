a62a7ca7c87b0bb6026ac819f102e804ee85ec0e
Sunstrike <sunstrike@azurenode.net>
2013-05-31 14:07:55 +0100

    Fix configuration loader. Fixes #2.

--- src/io/sunstrike/islandbuilder/helpers/IsBlConfig.scala
+++ src/io/sunstrike/islandbuilder/helpers/IsBlConfig.scala
@@ -31,11 +31,15 @@ object IsBlConfig {
      * @param config Forge Configuration object
      */
     def loadForgeConfig(config:Configuration) {
+        config.load()
+
         block_FlIsBuilder_id = config.getBlock("FlIsBuilder", block_FlIsBuilder_id).getInt
         block_FlIsBuilder = new FlIsBuilder(block_FlIsBuilder_id, Material.anvil)
 
         item_FlIsModule_id = config.getBlock("FlIsModule", item_FlIsModule_id).getInt
         item_FlIsModule = new FlIsModule(item_FlIsModule_id)
+
+        config.save()
 
         logger.info("[IsBlConfig] Loaded configuration.")
     }
